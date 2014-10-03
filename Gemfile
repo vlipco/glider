b61af546e8cb5694731a5a2e190e7a66033851f7
David Pelaez T <pelaez89@gmail.com>
2014-10-03 21:17:13 +0000

    updated dependencies for testing

--- Gemfile
+++ Gemfile
@@ -3,9 +3,17 @@ source "https://rubygems.org"
 gem 'aws-sdk', '< 2.0'
 gem 'activesupport'
 gem 'json'
-# Add dependencies to develop your gem here.
-# Include everything needed to run rake, tests, features, etc.
+
 group :development do
   gem "bundler", "~> 1.0"
   gem "jeweler", "~> 1.8.7"
+end
+
+
+group :test do
+	gem 'rspec'
+	#gem 'vcr'
+	#gem 'webmock', '>= 1.8.0', '< 1.16'
+	gem 'pry'
+    #gem 'httpclient'
 end
--- lib/glider/component.rb
+++ lib/glider/component.rb
@@ -2,108 +2,108 @@
 
 module Glider
 
-	class Component
+    class Component
 
-		attr_reader :task, :event
+        attr_reader :task, :event
 
-		def initialize(task, event=nil, completed_event=nil, control=nil)
-			@task = task
-			@event = event
-			@completed_event = completed_event
-			@control = control
-		end
+        def initialize(task, event=nil, completed_event=nil, control=nil)
+            @task = task
+            @event = event
+            @completed_event = completed_event
+            @control = control
+        end
 
-		def activity(name, version)
-			{name: name.to_s, version: version.to_s}
-		end
+        def activity(name, version)
+            {name: name.to_s, version: version.to_s}
+        end
 
-		class << self
+        class << self
 
-			attr_reader :before_polling_hook, :after_polling_hook
+            attr_reader :before_polling_hook, :after_polling_hook
 
-			# registed a polling hook
-			def before_polling(&block)
-				@before_polling_hook = block
-			end
+            # registed a polling hook
+            def before_polling(&block)
+                @before_polling_hook = block
+            end
 
-			# registed a polling hook
-			def after_polling(&block)
-				@after_polling_hook = block
-			end
+            # registed a polling hook
+            def after_polling(&block)
+                @after_polling_hook = block
+            end
 
-			# handles the exit flag differently for forks and threads
-			def time_to_exit
-				ProcessManager.use_forking ? @time_to_exit : Thread.current[:time_to_exit]
-			end
+            # handles the exit flag differently for forks and threads
+            def time_to_exit
+                ProcessManager.use_forking ? @time_to_exit : Thread.current[:time_to_exit]
+            end
 
-			def task_lock!
-				#Glider.logger.info "=> Starting task: #{Thread.current[:x]}"
-				Thread.current[:in_task] = true
-				@in_task = true
-				yield
-			ensure
-				@in_task = false
-				Thread.current[:in_task] = false
-				execute_exit if time_to_exit # in case an exit signal was received during task processing
-			end
+            def task_lock!
+                #Glider.logger.info "=> Starting task: #{Thread.current[:x]}"
+                Thread.current[:in_task] = true
+                @in_task = true
+                yield
+            ensure
+                @in_task = false
+                Thread.current[:in_task] = false
+                execute_exit if time_to_exit # in case an exit signal was received during task processing
+            end
 
-			def graceful_exit
-				if ProcessManager.use_forking
-					if @in_task
-						@time_to_exit = true
-					else
-						execute_exit
-					end
-				else
-					if Thread.current[:in_task]
-						Thread.current[:time_to_exit] = true
-					else
-						execute_exit
-					end
-				end
-			end
+            def graceful_exit
+                if ProcessManager.use_forking
+                    if @in_task
+                        @time_to_exit = true
+                    else
+                        execute_exit
+                    end
+                else
+                    if Thread.current[:in_task]
+                        Thread.current[:time_to_exit] = true
+                    else
+                        execute_exit
+                    end
+                end
+            end
 
-			def execute_exit
-				if ProcessManager.use_forking
-					Process.exit! 0
-				else
-					#puts "Killing #{Thread.current}"
-					Thread.current.exit
-				end
-			end
+            def execute_exit
+                if ProcessManager.use_forking
+                    Process.exit! 0
+                else
+                    #puts "Killing #{Thread.current}"
+                    Thread.current.exit
+                end
+            end
 
-			def register_signal_handlers
-				if ProcessManager.use_forking
-					Signal.trap('USR1') {graceful_exit}
-				end
-			end
+            def register_signal_handlers
+                if ProcessManager.use_forking
+                    Signal.trap('USR1') {graceful_exit}
+                end
+            end
 
-			def swf
-				@swf ||= AWS::SimpleWorkflow.new
-			end
+            def swf
+                @swf ||= AWS::SimpleWorkflow.new
+            end
 
-			# both setter and getter
-			def workers(workers_count=nil)
-				workers_count ? @workers = workers_count : @workers ||= 1
-			end
+            # both setter and getter
+            def workers(workers_count=nil)
+                workers_count ? @workers = workers_count : @workers ||= 1
+            end
 
-			# both setter and getter
-			def domain(domain_name=nil, retention_period: 10)
-				if domain_name
-					begin
-						@domain = swf.domains[domain_name.to_s]
-						@domain.status
-					rescue AWS::SimpleWorkflow::Errors::UnknownResourceFault => e
-						# create it if necessary
-						@domain = swf.domains.create(domain_name.to_s, retention_period)
-					end
-				else
-					@domain
-				end
-			end
+            # both setter and getter
+            def domain(domain_name=nil, retention_period: 10)
+                if domain_name
+                    begin
+                        @domain = swf.domains[domain_name.to_s]
+                        @domain.status
+                    rescue AWS::SimpleWorkflow::Errors::UnknownResourceFault => e
+                        # create it if necessary
+                        @domain = swf.domains.create(domain_name.to_s, retention_period)
+                    end
+                else
+                    @domain
+                end
+            end
 
-		end
+        end
 
-	end
+    end
 
 end
